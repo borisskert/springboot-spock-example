1ee8ffe28e29a76fe14d8dc4b02c61d86322ed66
Boris Skert <boris.skert@gmail.com>
2021-04-20 09:09:00 +0200

    Clean up project

--- src/main/java/de/borisskert/spring/spockexample/Application.java
+++ src/main/java/de/borisskert/spring/spockexample/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpockExampleApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpockExampleApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
